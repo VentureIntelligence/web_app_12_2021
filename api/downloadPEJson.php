6fe5ebc86c453e08e22b7d16db6ea14ac05fe395
Krishna <krishjan95@gmail.com>
2020-12-22 16:14:59 +0530

    changes on the api

---
+++ api/downloadPEJson.php
@@ -0,0 +1,17 @@
+<?php
+	require_once '../dbconnectvi.php';
+	$DB = new dbInvestments();
+	session_start();
+	if( isset( $_POST ) ) {
+		$jsonObj = /*json_decode( */$_POST[ 'jsonObj' ]/* )*/;
+		/*$jsonObj = (array) $jsonObj;
+		$cinArray = array_keys( $jsonObj );
+		print_r( json_encode( $jsonObj ) );*/
+
+		header('Content-Description: File Transfer');
+    	header('Content-Type: application/force-download');
+		header('Content-disposition: attachment; filename=PE_API.json');
+		echo $jsonObj;
+		exit;
+	}
+?>
