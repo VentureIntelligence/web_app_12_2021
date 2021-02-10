26de8efd6e167f918a5a76172a8dd5948bd53e19
Krishna <krishjan95@gmail.com>
2021-02-10 12:03:47 +0530

    T-1063&T-1064 changes

--- cfsnew/leftpanel.php
+++ cfsnew/leftpanel.php
@@ -2,7 +2,7 @@
 $StateWhere = "state_CountryID_FK = 113";
 $order7="state_name asc";
 $template->assign("state" , $state->getState($StateWhere,$order7));
-
+$template->assign("iocstate" , $state->getiocState());
 //pr($authAdmin);
 
 //pr($_SESSION);
@@ -222,7 +222,7 @@ $StateWhere = "state_CountryID_FK = 113";
 
 $states=$state->getState($StateWhere,$order7);
 $template->assign("state" , $states);
-
+$template->assign("iocstate" , $state->getiocState());
  if ($_REQUEST['answer']['Region']!="" && $_REQUEST['answer']['State']=="") { 
             $region_where = '';
             foreach($_REQUEST['answer']['Region'] as $regions){
@@ -354,6 +354,9 @@ if(isset($_REQUEST['auditorname']) && $_REQUEST['auditorname']!='' ){
 }
 if(isset($_REQUEST['chargeaddress']) && $_REQUEST['chargeaddress']!=''){ 
     $fliterlist[$i++]=array('field'=>'#chargeaddress','key'=>'','value'=>'Charge Address');
+}
+if(isset($_REQUEST['answer']['City']) && $_REQUEST['answer']['City']!=''){ 
+    $fliterlist[$i++]=array('field'=>'#city','key'=>'','value'=>'City');
 }
 if(isset($_REQUEST['tagsearch_auto']) && $_REQUEST['tagsearch_auto']!=''){ 
     $tagsearch = "tag:" . trim($_REQUEST['tagsearch_auto']);
