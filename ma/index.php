64319bce6b3da3f4f0553ec654455a566b299126
Suganya <suganya.s@praniontech.com>
2021-03-24 16:16:42 +0530

    special char restriction issue fix

--- ma/index.php
+++ ma/index.php
@@ -4463,7 +4463,7 @@ div.token-input-dropdown{
     
     $('#popup_keyword').keyup(function() {
         var $th = $(this);
-        $th.val( $th.val().replace(/[^a-zA-Z0-9_ _']/g, function(str) { alert('You typed  ' + str + ' \n\nPlease use only letters, space and numbers.'); return ''; } ) );
+        $th.val( $th.val().replace(/[^a-zA-Z0-9_ _ &.']/g, function(str) { alert('You typed  ' + str + ' \n\nPlease use only letters, space and numbers.'); return ''; } ) );
     });
 });
 //Added for the T-931    
