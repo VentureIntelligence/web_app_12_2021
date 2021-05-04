5a72bf5868634e1ea3ba32bb9b098981429932ed
Suganya <suganya.s@praniontech.com>
2021-05-04 12:30:19 +0530

    crud newsletter

---
+++ adminvi/ajax_news_del.php
@@ -0,0 +1,26 @@
+<?php
+
+require("../dbconnectvi.php");
+$Db = new dbInvestments();
+ //session_save_path("/tmp");
+session_start();
+//print_r($_POST);
+if (session_is_registered("SessLoggedAdminPwd") && session_is_registered("SessLoggedIpAdd"))
+{
+    if( isset( $_POST ) ) {
+        $user_id = $_POST[ 'userid' ];
+        //$update = "DELETE FROM newsletter WHERE  id   = " . $user_id;
+        $update = "UPDATE newsletter SET is_deleted = 1 WHERE id = " . $user_id;
+
+        mysql_query( $update ) or die( mysql_error() );
+        echo 1;
+    } else {
+        echo 2;
+    }
+?>
+<?php
+
+} // if resgistered loop ends
+else
+    header( 'Location: ' . BASE_URL . 'admin.php' ) ;
+?>
