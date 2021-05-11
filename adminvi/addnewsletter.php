55ff158b00008b186ea34896d97e6cd6f1dcf877
Suganya <suganya.s@praniontech.com>
2021-05-11 15:41:47 +0530

    align css

--- adminvi/addnewsletter.php
+++ adminvi/addnewsletter.php
@@ -21,10 +21,10 @@ if (session_is_registered("SessLoggedAdminPwd") && session_is_registered("SessLo
         $publish_at = $_POST[ 'publish_at' ];
         $keyword="";
         $keyword=$_GET['id'];
-        $date  = "$publish_at";
+        //$date  = "$publish_at";
 
-        $dt   = new DateTime($date);
-        $epochtime= $dt->getTimestamp();
+        //$dt   = new DateTime($date);
+        //$epochtime= $dt->getTimestamp();
         $sel = "SELECT * from newsletter where id='".$keyword."'";
         $res = mysql_query( $sel ) or die( mysql_error() );
         $numrows = mysql_num_rows( $res );
@@ -32,8 +32,8 @@ if (session_is_registered("SessLoggedAdminPwd") && session_is_registered("SessLo
             $result = mysql_fetch_array( $res );
            
         } else {
-            $insert = "INSERT INTO newsletter ( category, heading, summary, targetcmp_website, vi_database, publish_at,created_on )
-                        VALUES( '" . $category . "', '" . $heading . "', '" . $summary . "', '" . $Targetcmp_website . "', '" . $vi_database . "', '" . $epochtime . "','" . $createdOn . "' )";
+            $insert = "INSERT INTO newsletter ( category, heading, summary, targetcmp_website, vi_database, published_at,created_on )
+                        VALUES( '" . $category . "', '" . $heading . "', '" . $summary . "', '" . $Targetcmp_website . "', '" . $vi_database . "', '" . $publish_at . "','" . $createdOn . "' )";
            //echo $insert;exit();
            if( mysql_query( $insert ) ) {
                 $lastInsertId = mysql_insert_id();
