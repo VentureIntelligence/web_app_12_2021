f8df4f62f26505eea0dcf253d55c36e2d184e3fe
Suganya <suganya.s@praniontech.com>
2021-07-16 17:47:43 +0530

    delete user also

--- cfsnew/admin/deletefullapi.php
+++ cfsnew/admin/deletefullapi.php
@@ -12,14 +12,17 @@ if(!$_SESSION['business']['Auth']){
 
 $fullapi_user_id =  $_GET["pid"];
 
+
+ $getUserid = mysql_query("SELECT * FROM `fullapi_user` WHERE fullapi_user_id=$fullapi_user_id");
+ $user_id = mysql_fetch_object($getUserid)->user_id;
+
  $query = "DELETE FROM fullapi_user WHERE fullapi_user_id = $fullapi_user_id";
+ $delete_rec = mysql_query($query);
 
- //echo $query;exit();
-
-     $api_partners_delete =  mysql_query ($query);
-
-    if($api_partners_delete == 1)
-    {
+ if($delete_rec == 1)
+ {
+     $query1 = "DELETE FROM external_fullapi_users WHERE `fullapi_user_id`=$user_id";
+     $api_users = mysql_query($query1);
     ?>
     <script> alert('Deleted Succesfully'); 
         window.location = './fullapi/users-fullapilist.php';</script>
--- cfsnew/admin/deletenewspartner.php
+++ cfsnew/admin/deletenewspartner.php
@@ -10,16 +10,18 @@ if(!$_SESSION['business']['Auth']){
 	die();
 }
 
-$partner_id =  $_GET["pid"];
+ $partner_id =  $_GET["pid"];
+
+ $getUserid = mysql_query("SELECT * FROM `news_api_partner` WHERE partner_id=$partner_id");
+ $user_id = mysql_fetch_object($getUserid)->user_id;
 
  $query = "DELETE FROM news_api_partner WHERE partner_id = $partner_id";
+ $delete_rec = mysql_query($query);
 
- //echo $query;exit();
-
-     $api_partners_delete =  mysql_query ($query);
-
-    if($api_partners_delete == 1)
-    {
+ if($delete_rec == 1)
+ {
+     $query1 = "DELETE FROM news_api_users WHERE `user_id`=$user_id";
+     $api_users = mysql_query($query1);
     ?>
     <script> alert('Deleted Succesfully'); 
     window.location = './news/partners-list.php';</script>
