a92fa3e3d8973534efe133c4fd705ec899a07625
Narasimman <narasimman.p@praniontech.com>
2021-09-30 20:27:04 +0530

    Export & Inv

--- dealsnew/index.php
+++ dealsnew/index.php
@@ -4913,6 +4913,7 @@ if ($type != 1) {
             <!--input class="postlink" type="hidden" name="numberofcom" value="<?php echo $totalInv; ?>"-->
             <form name="pelisting" id="pelisting"  method="post" action="exportinvdeals.php">
             <input type="hidden" name="valInfo" value="<?php echo $valInfo;?>" >
+            
 
             <input type="hidden" name="txtsearchon" value="1" >
             <input type="hidden" name="vcflagValue" value=<?php echo $vcflagValue; ?> >
