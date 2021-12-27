631fb7d44e8ee6476f9a428ebe9edb0be080fa93
pranionvijay <vijayakumar.k@praniontech.com>
2021-12-27 16:04:38 +0530

    VI5-T1329 changes

--- dealsnew/index.php
+++ dealsnew/index.php
@@ -223,6 +223,10 @@ include 'checklogin.php';
                 header("Location:".BASE_URL."dealsnew/index.php?value=1/");
               
                  $_SESSION['VCcheckflag'] = 1;
+
+                 if($_SESSION['vconly']==1 && !isset($_GET['value'])) {
+                    header("Location:".BASE_URL."dealsnew/index.php?value=1/");
+                }
             }?>
         
     
